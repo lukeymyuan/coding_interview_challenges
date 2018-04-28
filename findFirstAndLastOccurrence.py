20b2522661be32321c7f31461b822118651a1bed
Luke <lukelalalala@gmail.com>
2018-04-28 15:47:58 +1000

    Update findFirstAndLastOccurrence.py

--- findFirstAndLastOccurrence.py
+++ findFirstAndLastOccurrence.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# Given a sorted array with possibly duplicate elements, 
+# the task is to find indexes of first and last occurrences of an element x in the given array.
+# https://www.geeksforgeeks.org/find-first-and-last-positions-of-an-element-in-a-sorted-array/
 
 def first(arr,x):
     low, high = 0, len(arr)
